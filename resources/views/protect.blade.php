19359d266cd853af7fc7b3a48c975a913bf71ea7
Alan Jenkins <alan@functionfirst.co.uk>
2021-06-24 13:35:42 +0100

    Fix issue with button layout on safari

--- resources/views/protect.blade.php
+++ resources/views/protect.blade.php
@@ -8,7 +8,7 @@
         class="mb-6"
     >
         Based on the evidence Charger recommends that you continue to
-        <button class="flex border shadow-md border-white text-white rounded-full px-4 py-2 hover:border-blue-100 hover:text-blue-100">
+        <button class="flex items-center border shadow-md border-white text-white rounded-full px-4 py-2 hover:border-blue-100 hover:text-blue-100">
             <x-icon.shield-exclamation class="w-5 mr-2" />
             Defend this dispute
         </button>
