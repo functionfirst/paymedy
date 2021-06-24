f3be2dcb37feb1fce52452c66ac091a3237388b8
Alan Jenkins <alan@functionfirst.co.uk>
2021-06-24 13:34:23 +0100

    Fix layout issue on Safari

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,5 +1,5 @@
 <div
-    class="alert flex bg-yellow-200 text-yellow-800 px-4 py-2 rounded-full"
+    class="alert flex items-center bg-yellow-200 text-yellow-800 px-4 py-2 rounded-full"
     role="alert"
 >
     <x-icon.exclamation-circle class="w-5 mr-2" />
