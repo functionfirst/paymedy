c09c951f4424fcc06ab9a5e61729530bcd2691d5
Alan Jenkins <alan@functionfirst.co.uk>
2021-06-26 12:02:59 +0100

    Remove SIdebarServiceProvider
    Makes more sense to have it at a page controller level rather than the app level.

--- app/Http/Controllers/ProtectController.php
+++ app/Http/Controllers/ProtectController.php
@@ -6,11 +6,38 @@ use App\Http\Controllers\Controller;
 
 class ProtectController extends Controller
 {
+    protected $sidebar = [
+        [
+            'icon' => 'icon.clock',
+            'name' => 'Case activity',
+            'route' => 'home',
+            'active' => true
+        ],
+        [
+            'icon' => 'icon.template',
+            'name' => 'Templates',
+            'route' => 'home',
+            'active' => false
+        ],
+        [
+            'icon' => 'icon.chart-pie',
+            'name' => 'Performance',
+            'route' => 'home',
+            'active' => false
+        ],
+        [
+            'icon' => 'icon.bell',
+            'name' => 'Notifications',
+            'route' => 'home',
+            'active' => false
+        ]
+    ];
+
     public function show()
     {
         return view('protect', [
             'navigation' => app('navigation'),
-            'sidebar' => app('sidebar')
+            'sidebar' => $this->sidebar,
         ]);
     }
 }
--- config/app.php
+++ config/app.php
@@ -175,7 +175,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\NavigationServiceProvider::class,
-        App\Providers\SidebarServiceProvider::class,
 
     ],
 
